60bb135594a0af2d7e488e813a826a680da3690f
LeCrovVv <117682185+lecrovvv@users.noreply.github.com>
2024-05-27 09:55:18 +0300

    Update fixArc.bat

--- fixArc.bat
+++ fixArc.bat
@@ -3,5 +3,5 @@ taskkill /IM Arc.exe /F
 echo Tried to kill an Arc process.
 rmdir /s /q C:\Users\%username%\AppData\Local\Packages\TheBrowserCompany.Arc_ttt1ap7aakyb4\LocalCache\Local\firestore\Arc
 echo Sucessfully patched, launching...
-start C:\Users\weird\AppData\Local\Microsoft\WindowsApps\Arc.exe
+start C:\Users\%username%\AppData\Local\Microsoft\WindowsApps\Arc.exe
 pause
