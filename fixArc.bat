4a4975341a4805e369d9aaf657eed032fb5ca77b
LeCrovVv <117682185+lecrovvv@users.noreply.github.com>
2024-05-27 09:41:45 +0300

    Add files via upload

---
+++ fixArc.bat
@@ -0,0 +1,7 @@
+@echo off
+taskkill /IM Arc.exe /F
+echo Tried to kill an Arc process.
+rmdir /s /q C:\Users\%username%\AppData\Local\Packages\TheBrowserCompany.Arc_ttt1ap7aakyb4\LocalCache\Local\firestore\Arc
+echo Sucessfully patched, launching...
+start C:\Users\weird\AppData\Local\Microsoft\WindowsApps\Arc.exe
+pause
